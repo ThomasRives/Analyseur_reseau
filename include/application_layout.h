badefa03f2ed2fa07755b22f61cd4461b6c3c10b
ThomasRives <thomas.rives29@gmail.com>
2020-11-02 18:41:54 +0100

    facto ICMP

--- include/application_layout.h
+++ include/application_layout.h
@@ -4,26 +4,30 @@
 /*
 Port|UDP|TCP|decription
 -----------------------
-9   | 1 | 1 | Disard
-20  | 0 | 1 | ftp-data
-21  | 1 | 1 | ftp
+9   | 1 | 1 | Disar
+ 21  | 1 | 1 | ftp TODO
 22  | 1 | 1 | ssh
-23  | 0 | 1 | telnet
-25  | 0 | 1 | smtp
+23  | 0 | 1 | telnet TODO
+25  | 0 | 1 | smtp TODO
 42  | 1 | 1 | Service de noms
-50  | 1 | 1 | Remote Mail Checking
-53  | 1 | 1 | Résolution de nom par DNS
-67  | 1 | 0 | Service Bootp Protocol
-68  | 1 | 0 | Bootstrap Client
-69  | 1 | 0 | TFTP
-80  | 0 | 1 | HTTP
-101 | 0 | 1 | NIC Host Name
+50  | 1 | 1 | Remote Mail Checking 
+53  | 1 | 1 | Résolution de nom par DNS TODO
+67  | 1 | 0 | Service Bootp Protocol TODO
+68  | 1 | 0 | Bootstrap Client 
+69  | 1 | 0 | TFTP 
+80  | 0 | 1 | HTTP TODO
+101 | 0 | 1 | NIC Host Name 
 115 | 0 | 1 | SFTP
 443 | 0 | 1 | HTTPS
 514 | 0 | 1 | Shell remote
 514 | 1 | 0 | Unix Syslog
 521 | 1 | 0 | Routing Info Protocol for IPv6
 992 | 1 | 1 | Telnets via SSL/TLS
+
+//LDAP
+//IMAP
+//POP
+
 
 Use destination port to know the protocol
 */
--- include/transport_layout.h
+++ include/transport_layout.h
@@ -41,6 +41,63 @@ void udp_header_analyze(const u_char *packet);
 void icmp_header_analyze(const u_char *packet);
 
 /**
+ * @brief Print the type/code informations for icmp.
+ * 
+ * @param type: the type of the icmp packet.
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp_type_code(uint8_t type, uint8_t code);
+
+/**
+ * @brief Print the code if the type is "Destination Unreachable"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp_dest_unreach_code(uint8_t code);
+
+/**
+ * @brief Print the code if the type is "Redirect"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp_redirect_code(uint8_t code);
+
+/**
+ * @brief Print the code if the type is "Router Advertisement"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp_rout_ad_code(uint8_t code);
+
+/**
+ * @brief Print the code if the type is "Time Exceeded"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp_time_exc_code(uint8_t code);
+
+/**
+ * @brief Print the code if the type is "Parameter Problem"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp_par_prob_code(uint8_t code);
+
+/**
+ * @brief Print the code if the type is "Photuris"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp_photuris_code(uint8_t code);
+
+/**
+ * @brief Print the code if the type is "Extended Echo Reply"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp_ext_ech_rep_code(uint8_t code);
+
+/**
  * @brief Analyze the ICMPv6 header of the packet.
  * 
  * @param packet: the packet himself.
