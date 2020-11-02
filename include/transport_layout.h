d2c24f6b7eba36b5534473c5c209369999370180
ThomasRives <thomas.rives29@gmail.com>
2020-11-02 19:10:06 +0100

    facto ICMPv6

--- include/transport_layout.h
+++ include/transport_layout.h
@@ -49,49 +49,49 @@ void icmp_header_analyze(const u_char *packet);
 void print_icmp_type_code(uint8_t type, uint8_t code);
 
 /**
- * @brief Print the code if the type is "Destination Unreachable"
+ * @brief Print the icmp code if the type is "Destination Unreachable"
  * 
  * @param code: the code of the icmp packet.
  */
 void print_icmp_dest_unreach_code(uint8_t code);
 
 /**
- * @brief Print the code if the type is "Redirect"
+ * @brief Print the icmp code if the type is "Redirect"
  * 
  * @param code: the code of the icmp packet.
  */
 void print_icmp_redirect_code(uint8_t code);
 
 /**
- * @brief Print the code if the type is "Router Advertisement"
+ * @brief Print the icmp code if the type is "Router Advertisement"
  * 
  * @param code: the code of the icmp packet.
  */
 void print_icmp_rout_ad_code(uint8_t code);
 
 /**
- * @brief Print the code if the type is "Time Exceeded"
+ * @brief Print the icmp code if the type is "Time Exceeded"
  * 
  * @param code: the code of the icmp packet.
  */
 void print_icmp_time_exc_code(uint8_t code);
 
 /**
- * @brief Print the code if the type is "Parameter Problem"
+ * @brief Print the icmp code if the type is "Parameter Problem"
  * 
  * @param code: the code of the icmp packet.
  */
 void print_icmp_par_prob_code(uint8_t code);
 
 /**
- * @brief Print the code if the type is "Photuris"
+ * @brief Print the icmp code if the type is "Photuris"
  * 
  * @param code: the code of the icmp packet.
  */
 void print_icmp_photuris_code(uint8_t code);
 
 /**
- * @brief Print the code if the type is "Extended Echo Reply"
+ * @brief Print the icmp code if the type is "Extended Echo Reply"
  * 
  * @param code: the code of the icmp packet.
  */
@@ -103,5 +103,33 @@ void print_icmp_ext_ech_rep_code(uint8_t code);
  * @param packet: the packet himself.
  */
 void icmpv6_header_analyze(const u_char *packet);
+
+/**
+ * @brief Print the icmpv6 code if the type is "Destination Unreachable"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp6_dest_unreach_code(uint8_t code);
+
+/**
+ * @brief Print the icmpv6 code if the type is "Time Exceeded"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmp6_time_exc_code(uint8_t code);
+
+/**
+ * @brief Print the icmpv6 code if the type is "Parameter Problem"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmpv6_par_prob_code(uint8_t code);
+
+/**
+ * @brief Print the icmpv6 code if the type is "Router Renumbering"
+ * 
+ * @param code: the code of the icmp packet.
+ */
+void print_icmpv6_rout_rem_code(uint8_t code);
 
 #endif //TRANSPORT_LAYOUT_H
